d1fb330488d44a80a9486e08ef15c1897ab3de17
salmansalmanov <salmanov2904@gmail.com>
2026-08-10 15:19:04 +0400

    docs: add Swagger documentation for FileController

--- src/main/java/com/salman/week1/controller/FileController.java
+++ src/main/java/com/salman/week1/controller/FileController.java
@@ -2,24 +2,39 @@ package com.salman.week1.controller;
 
 import com.salman.week1.model.dto.response.UploadResponse;
 import com.salman.week1.service.abstraction.FileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "File Management", description = "REST APIs for managing file uploads")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/files")
 public class FileController {
     private final FileService fileService;
 
+    @Operation(summary = "Upload a file", description = "Uploads a file to the system and returns information about the uploaded file.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "File successfully uploaded"),
+            @ApiResponse(responseCode = "400", description = "Invalid file or request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error while uploading the file")
+    })
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<UploadResponse> upload(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<UploadResponse> upload(
+            @Parameter(
+                    description = "File to be uploaded",
+                    required = true
+            )
+            @RequestParam("file") MultipartFile file
+    ) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(fileService.upload(file));
